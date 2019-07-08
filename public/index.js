ee67728dc6c3fa49b7a79f3b78d5e19ab4d2e206
alaynapuck28 <you@example.com>
2019-07-08 10:41:56 -0500

    style changes

--- public/index.js
+++ public/index.js
@@ -1,25 +1,24 @@
 let detailsButton = (document.createElement("button").value = "More Details!");
-let itemQuantity=1;
+let itemQuantity = 1;
 
 function Products(products) {
-  document.getElementById("products").innerHTML = products.map(
-    (product, index) => {
-      
-     let amount= sessionStorage.getItem(product.id);
-     if(amount){
-       addToCart(product.id);
-     }
+  document.getElementById("products").innerHTML = products
+    .map((product, index) => {
+      let amount = sessionStorage.getItem(product.id);
+      if (amount) {
+        addToCart(product.id);
+      }
 
-     return `<div class="productList">
+      return `<div class="productList">
      
      <p>${product.name}</p>
      <img src="${product.imgUrl}" alt="image of ${product.name}">
-     <p> Product ID: ${product.id}</p>
      <p>${product.description}</p>
 
-
-     <p><button id="moredetails" onclick = "moreDetails(${product.id})">${detailsButton}</button></p>
-     <p><button onclick="addToCartAndStore(${product.id})">Add to Cart</button></p>
+     <button id="moreDetailsButton" onclick = "moreDetails(${
+       product.id
+     })">${detailsButton}</button>
+     <br>
      <select id=select-${index} onchange="getValue(${index})">
                     <option value="0">--Quantity--</option>
                     <option value="1">1</option>
@@ -33,108 +32,101 @@ function Products(products) {
                     <option value="9">9</option>
                     <option value="10">10</option>
                 </select>
+     <button onclick="addToCartAndStore(${product.id})">Add to Cart</button>
+     
                 </div>
                 <hr>`;
-     
-     }
-   ).join(' ');
- }
+    })
+    .join(" ");
+}
 
 window.onload = Products(products);
 
-function Search() {
 
+//*************Search Function ********/
+function Search() {
+  //takes in user input
   let input = document.getElementById("searchBox");
   let filter = input.value;
-  let foundProd = [];
+  let foundProdArr = [];
   products.map((item, index) => {
+    //splits up each word in item name and put into an array
     let nameArray = item.name.toLowerCase().split(" ");
     nameArray.filter(name => {
+      //if the name matches user input then push into array
       if (name == filter) {
-        foundProd.push(item);
+        foundProdArr.push(item);
       }
     });
   });
-  return Products(foundProd);
+  //origianl function run displays new array of found products
+  return Products(foundProdArr);
 }
 
-// function search() {
-//   let searchWord = document.getElementById("searchBox").value;
-//   let filteredProducts = products.filter(p => p.name === searchWord)
-
-//   Products(filteredProducts);
-// }
-
-
+//**********Show more Details of Specific Product********/
 function moreDetails(prodId) {
-  let foundProd = products.find(p => p.id === prodId);
+  let foundProduct = products.find(p => p.id === prodId);
 
-  document.getElementById("productDetail").innerHTML = 
-    `<div id="moreDetailsCard">
-    <p>${foundProd.name}</p>
-     <p> Ratings: ${foundProd.rating}/5</p>
-     <img src="${foundProd.imgUrl}" alt="image of ${foundProd.name}">
-     <p> Product ID: ${foundProd.id}</p>
-     <p>${foundProd.description}</p>
+  document.getElementById(
+    "productDetail"
+  ).innerHTML = `<div id="moreDetailsCard">
+    <p>${foundProduct.name}</p>
+     <p> Ratings: ${foundProduct.rating}/5</p>
+     <img src="${foundProduct.imgUrl}" alt="image of ${foundProduct.name}">
+     <p> Product ID: ${foundProduct.id}</p>
+     <p>${foundProduct.description}</p>
      <hr></hr>
      </div>`;
 }
 
-
-
-
-/******************* Call both AddToCart and Store Functions *********************/
-function addToCartAndStore(id){
+/************Call both AddToCart and Store Functions ****************/
+function addToCartAndStore(id) {
   //if item in storage already dont add again
-  if(sessionStorage.getItem(id)) return;
+  if (sessionStorage.getItem(id)) return;
   addToCart(id);
   Storage(id);
-  
 }
 
-
 //***************Session Storage*************/
-function Storage(id){
-  sessionStorage.setItem(id, 1)
-} 
-
-
+function Storage(id) {
+  sessionStorage.setItem(id, 1);
+}
 
 //*********************Add to Cart *************/
 function addToCart(id) {
-  
-  let cartItemsL= document.getElementById('cartitems');
+  let cartItemsL = document.getElementById("cartitems");
   let productId = products.find(function(product) {
     return product.id == id;
   });
-  let cartItems = cartItemsL.innerHTML+ `<li>${productId.name}: ${productId.price} (${itemQuantity})</li>`;
+  let cartItems =
+    cartItemsL.innerHTML +
+    `<li>${productId.name}: ${productId.price} (${itemQuantity})</li>`;
 
   document.getElementById("cartitems").innerHTML = cartItems;
 }
 
-
-
 //quantity function in progress
 const getValue = index => {
-  const selectIndex = document.getElementById(`select-${index}`)
-  itemQuantity= Number(selectIndex.value)
+  const selectIndex = document.getElementById(`select-${index}`);
+  itemQuantity = Number(selectIndex.value);
   console.log(selectIndex.value);
   return itemQuantity;
-}
+};
 
 //category
 //function called when category is changed via dropdown menu on homepage
 
-function filterCategory(cat){
+function filterCategory(cat) {
   //if all is selected, displays all products
-  if (cat.toLowerCase() === 'all'){
-      Products(products);
+  if (cat.toLowerCase() === "all") {
+    Products(products);
   }
   //filters products depending on category selected
-  else{
-      let filteredProducts = products.filter(prod => 
-          prod.category === cat.toLowerCase());
-      Products(filteredProducts);
+  else {
+    let filteredProducts = products.filter(
+      prod => prod.category === cat.toLowerCase()
+    );
+    Products(filteredProducts);
   }
 }
 
@@ -153,5 +145,3 @@ function filterCategory(cat){
 //     // moreDetailsButton[i].style.display="none";
 //   }
 // }
-
-
